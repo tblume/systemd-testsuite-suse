2415ea7fe517aeb1bb03294cd938a6e1b4e51b2d
Thomas Blume <thomas.blume@suse.com>
2019-03-06 10:04:39 +0100

    TEST-03-JOBS: remove qemu from testrun

--- test/TEST-03-JOBS/test.sh
+++ test/TEST-03-JOBS/test.sh
@@ -4,17 +4,34 @@
 set -e
 TEST_DESCRIPTION="Job-related tests"
 
+export TEST_BASE_DIR=/var/opt/systemd-tests/test
 . $TEST_BASE_DIR/test-functions
 
+test_run() {
+    ret=1
+    systemctl daemon-reload
+    systemctl start testsuite.service || return 1
+    systemctl status --full testsuite.service
+    if [ -z "$TEST_NO_NSPAWN" ]; then
+        if run_nspawn; then
+            check_result_nspawn || return 1
+        else
+            dwarn "can't run systemd-nspawn, skipping"
+        fi
+    fi
+    test -s /failed && ret=$(($ret+1))
+    [[ -e /testok ]] && ret=0
+    return $ret
+}
+
 test_setup() {
-    create_empty_image
     mkdir -p $TESTDIR/root
-    mount ${LOOPDEV}p1 $TESTDIR/root
+    initdir=$TESTDIR/root
+    STRIP_BINARIES=no
 
     # Create what will eventually be our root filesystem onto an overlay
     (
         LOG_LEVEL=5
-        eval $(udevadm info --export --query=env --name=${LOOPDEV}p2)
 
         setup_basic_environment
 
@@ -25,21 +42,34 @@ Description=Testsuite service
 After=multi-user.target
 
 [Service]
-ExecStart=/test-jobs.sh
+ExecStart=$TESTDIR/nspawn-root/test-jobs.sh
+ExecStartPost=/bin/sh -x -c 'echo -e "\nfailed:" > /failed; systemctl --state=failed --no-pager >> /failed; echo -e "\ntestresult:\nOK" > /testok'
 Type=oneshot
 EOF
-
         # copy the units used by this test
+        cp $initdir/etc/systemd/system/testsuite.service /etc/systemd/system/testsuite.service
         cp $TEST_BASE_DIR/{hello.service,sleep.service,hello-after-sleep.target,unstoppable.service} \
             $initdir/etc/systemd/system
         cp test-jobs.sh $initdir/
 
+        for service in hello.service sleep.service hello-after-sleep.target unstoppable.service testsuite.service; do
+            cp $initdir/etc/systemd/system/$service /etc/systemd/system/
+        done
         setup_testsuite
     ) || return 1
-    setup_nspawn_root
 
-    ddebug "umount $TESTDIR/root"
-    umount $TESTDIR/root
+    setup_nspawn_root
+    rm -r $TESTDIR/root
+}
+
+test_cleanup() {
+    for service in hello.service sleep.service hello-after-sleep.target unstoppable.service testsuite.service; do
+         rm /etc/systemd/system/$service
+    done
+    [[ -e /testok ]] && rm /testok
+    [[ -e /failed ]] && rm /failed
+    return 0
+
 }
 
 do_test "$@"
