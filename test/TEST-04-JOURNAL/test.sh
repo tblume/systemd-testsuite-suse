7f390178f09a2a11009f1fafbab5b486ba288da3
Thomas Blume <thomas.blume@suse.com>
2019-03-07 09:58:24 +0100

    TEST-04-JOURNAL: remove qemu from testrun

--- test/TEST-04-JOURNAL/test.sh
+++ test/TEST-04-JOURNAL/test.sh
@@ -4,17 +4,38 @@
 set -e
 TEST_DESCRIPTION="Journal-related tests"
 
+export TEST_BASE_DIR=/var/opt/systemd-tests/test
 . $TEST_BASE_DIR/test-functions
 
+test_run() {
+    ret=1
+    systemctl daemon-reload
+    systemctl start testsuite.service || return 1
+    systemctl status --full testsuite.service
+    if [ -z "$TEST_NO_NSPAWN" ]; then
+        if run_nspawn; then
+            check_result_nspawn || return 1
+        else
+            dwarn "can't run systemd-nspawn, skipping"
+        fi
+    fi
+    test -s /failed && ret=$(($ret+1))
+    [[ -e /testok ]] && ret=0
+    return $ret
+}
+
+
+
+
+
 test_setup() {
-    create_empty_image
     mkdir -p $TESTDIR/root
-    mount ${LOOPDEV}p1 $TESTDIR/root
+    initdir=$TESTDIR/root
+    STRIP_BINARIES=no
 
     # Create what will eventually be our root filesystem onto an overlay
     (
         LOG_LEVEL=5
-        eval $(udevadm info --export --query=env --name=${LOOPDEV}p2)
 
         setup_basic_environment
 
@@ -25,7 +46,8 @@ Description=Testsuite service
 After=multi-user.target
 
 [Service]
-ExecStart=/test-journal.sh
+ExecStart=$TESTDIR/nspawn-root/test-journal.sh
+ExecStartPost=/bin/sh -x -c 'echo -e "\nfailed:" > /failed; systemctl --state=failed --no-pager >> /failed; echo -e "\ntestresult:\nOK" > /testok'
 Type=oneshot
 EOF
 
@@ -38,14 +60,29 @@ Type=simple
 ExecStart=/bin/sh -x -c 'while :; do printf "Hola\n" || touch /i-lose-my-logs; sleep 1; done'
 EOF
 
+        # copy the units used by this test
         cp test-journal.sh $initdir/
+        for service in forever-print-hola.service testsuite.service; do
+            cp $initdir/etc/systemd/system/$service /etc/systemd/system/
+        done
 
         setup_testsuite
     ) || return 1
-    setup_nspawn_root
 
-    ddebug "umount $TESTDIR/root"
-    umount $TESTDIR/root
+    setup_nspawn_root
+    rm -r $TESTDIR/root
 }
+
+test_cleanup() {
+
+    for service in forever-print-hola.service testsuite.service; do
+         rm /etc/systemd/system/$service
+    done
+    [[ -e /testok ]] && rm /testok
+    [[ -e /failed ]] && rm /failed
+    return 0
+}
+
+
 
 do_test "$@"
