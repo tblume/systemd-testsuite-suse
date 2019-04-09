b48266b1ed9ee105e17bec323717f6af4597c50c
Thomas Blume <thomas.blume@suse.com>
2019-04-09 09:58:03 +0200

    TEST-15-DROPIN: remove qemu from testrun

--- test/TEST-15-DROPIN/test.sh
+++ test/TEST-15-DROPIN/test.sh
@@ -5,9 +5,26 @@ set -e
 TEST_DESCRIPTION="Dropin tests"
 TEST_NO_QEMU=1
 
+export TEST_BASE_DIR=/var/opt/systemd-tests/test
 . $TEST_BASE_DIR/test-functions
 
+test_run() {
+    ret=1
+    systemctl daemon-reload
+    systemctl start testsuite.service || return 1
+    systemctl status --full testsuite.service
+    test -s /failed && ret=$(($ret+1))
+    [[ -e /testok ]] && ret=0
+    return $ret
+}
+
 test_setup() {
+        mkdir -p $TESTDIR/root
+        initdir=$TESTDIR/root
+        STRIP_BINARIES=no
+
+        LOG_LEVEL=5
+
         # create the basic filesystem layout
         setup_basic_environment
 
@@ -19,8 +36,8 @@ test_setup() {
         ln -s /dev/null $initdir/etc/systemd/system/systemd-resolved.service
 
         # import the test scripts in the rootfs and plug them in systemd
-        cp testsuite.service $initdir/etc/systemd/system/
-        cp test-dropin.sh    $initdir/
+        cp testsuite.service /etc/systemd/system/
+        cp test-dropin.sh    /
         setup_testsuite
 
         # create dedicated rootfs for nspawn (located in $TESTDIR/nspawn-root)
@@ -28,7 +45,13 @@ test_setup() {
 }
 
 test_cleanup() {
-        return 0
-}
+    for service in testsuite.service; do
+         rm /etc/systemd/system/$service
+    done
+    rm /test-dropin.sh
+    [[ -e /testok ]] && rm /testok
+    [[ -e /failed ]] && rm /failed
+    return 0
+ }
 
 do_test "$@"
