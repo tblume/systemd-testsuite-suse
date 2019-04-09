71f831ea4ea487cb3c1800a1ca97c11ca1ad8b89
Thomas Blume <thomas.blume@suse.com>
2019-04-09 12:58:39 +0200

    TEST-22-TMPFILES: remove qemu from testrun

--- test/TEST-22-TMPFILES/test.sh
+++ test/TEST-22-TMPFILES/test.sh
@@ -6,8 +6,25 @@ TEST_DESCRIPTION="Tmpfiles related tests"
 TEST_NO_QEMU=1
 
 . $TEST_BASE_DIR/test-functions
+. $TEST_BASE_DIR/test-functions
+
+test_run() {
+    ret=1
+    systemctl daemon-reload
+    systemctl start testsuite.service || return 1
+    systemctl status --full testsuite.service
+    test -s /failed && ret=$(($ret+1))
+    [[ -e /testok ]] && ret=0
+    return $ret
+}
 
 test_setup() {
+    mkdir -p $TESTDIR/root
+    initdir=$TESTDIR/root
+    STRIP_BINARIES=no
+
+    LOG_LEVEL=5
+
     # create the basic filesystem layout
     setup_basic_environment
     inst_binary mv
@@ -26,15 +43,25 @@ test_setup() {
     ln -fs /dev/null $initdir/etc/systemd/system/systemd-machined.service
 
     # setup the testsuite service
-    cp testsuite.service $initdir/etc/systemd/system/
+    cp testsuite.service /etc/systemd/system/
     setup_testsuite
 
-    mkdir -p $initdir/testsuite
-    cp run-tmpfiles-tests.sh $initdir/testsuite/
-    cp test-*.sh $initdir/testsuite/
+    mkdir -p /testsuite
+    cp run-tmpfiles-tests.sh /testsuite/
+    cp test-*.sh /testsuite/
 
     # create dedicated rootfs for nspawn (located in $TESTDIR/nspawn-root)
     setup_nspawn_root
 }
+
+test_cleanup() {
+    for service in testsuite.service; do
+         rm /etc/systemd/system/$service
+    done
+    rm -r /testsuite
+    [[ -e /testok ]] && rm /testok
+    [[ -e /failed ]] && rm /failed
+    return 0
+ }
 
 do_test "$@"
