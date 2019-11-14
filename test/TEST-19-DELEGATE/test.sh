49e8daf3fb81c72f31575533e02f9bdc48f71287
Thomas Blume <Thomas.Blume@suse.com>
2019-11-14 11:42:46 +0100

    TEST-19-DELEGATE: adapt for openQA tests

--- test/TEST-19-DELEGATE/test.sh
+++ test/TEST-19-DELEGATE/test.sh
@@ -3,26 +3,35 @@ set -e
 TEST_DESCRIPTION="test cgroup delegation in the unified hierarchy"
 TEST_NO_NSPAWN=1
 
+export TEST_BASE_DIR=/var/opt/systemd-tests/test
 . $TEST_BASE_DIR/test-functions
-QEMU_TIMEOUT=600
-UNIFIED_CGROUP_HIERARCHY=yes
+
+test_run() {
+    ret=1
+    systemctl daemon-reload
+    systemctl start testsuite.service || return 1
+    systemctl status --full testsuite.service
+    if [ -z "$TEST_NO_NSPAWN" ]; then
+        if run_nspawn; then
+            check_result_nspawn || return 1
+        else
+            dwarn "can't run systemd-nspawn, skipping"
+        fi
+    fi
+    test -s /failed && ret=$(($ret+1))
+    [[ -e /testok ]] && ret=0
+    return $ret
+}
 
 test_setup() {
-    create_empty_image_rootdir
+    mkdir -p $TESTDIR/root
+    initdir=$TESTDIR/root
+    STRIP_BINARIES=no
 
     (
         LOG_LEVEL=5
-        eval $(udevadm info --export --query=env --name=${LOOPDEV}p2)
 
         setup_basic_environment
-
-        # mask some services that we do not want to run in these tests
-        ln -fs /dev/null $initdir/etc/systemd/system/systemd-hwdb-update.service
-        ln -fs /dev/null $initdir/etc/systemd/system/systemd-journal-catalog-update.service
-        ln -fs /dev/null $initdir/etc/systemd/system/systemd-networkd.service
-        ln -fs /dev/null $initdir/etc/systemd/system/systemd-networkd.socket
-        ln -fs /dev/null $initdir/etc/systemd/system/systemd-resolved.service
-        ln -fs /dev/null $initdir/etc/systemd/system/systemd-machined.service
 
         # setup the testsuite service
         cat >$initdir/etc/systemd/system/testsuite.service <<EOF
@@ -30,15 +39,44 @@ test_setup() {
 Description=Testsuite service
 
 [Service]
-ExecStart=/bin/bash -x /testsuite.sh
+ExecStart=/bin/bash -x /testsuite.sh &>/testscript.out
+ExecStartPost=/bin/sh -x -c 'systemctl --state=failed --no-pager > /failed'
 Type=oneshot
-StandardOutput=tty
-StandardError=tty
+StandardOutput=kmsg
+StandardError=kmsg
 EOF
-        cp testsuite.sh $initdir/
+        cp testsuite.sh /
+
+        for service in testsuite.service; do
+            cp $initdir/etc/systemd/system/$service /etc/systemd/system/
+        done
 
         setup_testsuite
     )
+
+     # mask some services that we do not want to run in these tests
+    [[ -f /etc/systemd/system/systemd-hwdb-update.service ]] && ln -fs /dev/null /etc/systemd/system/systemd-hwdb-update.service
+    [[ -f /etc/systemd/system/systemd-journal-catalog-update.service ]] && ln -fs /dev/null /etc/systemd/system/systemd-journal-catalog-update.service
+    [[ -f /etc/systemd/system/systemd-networkd.service ]] && ln -fs /dev/null /etc/systemd/system/systemd-networkd.service
+    [[ -f /etc/systemd/system/systemd-networkd.socket ]] && ln -fs /dev/null /etc/systemd/system/systemd-networkd.socket
+    [[ -f /etc/systemd/system/systemd-resolved.service ]] && ln -fs /dev/null /etc/systemd/system/systemd-resolved.service
+    [[ -f /etc/systemd/system/systemd-machined.service ]] && ln -fs /dev/null /etc/systemd/system/systemd-machined.service
+
+   sed -i '/^[ !·······]*GRUB_CMDLINE_LINUX_DEFAULT.*/s/"$/ systemd.unified_cgroup_hierarchy=yes"/' /etc/default/grub
+   grub2-mkconfig -o /boot/grub2/grub.cfg || return 1
+}
+
+test_cleanup() {
+    for service in testsuite.service; do
+         rm /etc/systemd/system/$service
+    done
+    [[ -e /testok ]] && rm /testok
+    [[ -e /failed ]] && rm /failed
+    [[ -e /testscript.out ]] && rm /testscript.out
+    rm /testsuite.sh
+    sed -i '/^[ !·······]*GRUB_CMDLINE_LINUX_DEFAULT.*/s/ systemd.unified_cgroup_hierarchy=yes//' /etc/default/grub
+    grub2-mkconfig -o /boot/grub2/grub.cfg
+    return 0
 }
 
 do_test "$@"
