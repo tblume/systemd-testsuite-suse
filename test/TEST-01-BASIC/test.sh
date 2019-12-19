cbcad0c64c6569c27c3beb9626273d658d2e1384
Thomas Blume <Thomas.Blume@suse.com>
2019-12-19 08:35:58 +0100

    TEST-01-BASIC: adapt for SUSE openQA

--- test/TEST-01-BASIC/test.sh
+++ test/TEST-01-BASIC/test.sh
@@ -3,15 +3,33 @@ set -e
 TEST_DESCRIPTION="Basic systemd setup"
 RUN_IN_UNPRIVILEGED_CONTAINER=${RUN_IN_UNPRIVILEGED_CONTAINER:-yes}
 
+export TEST_BASE_DIR=/var/opt/systemd-tests/test
 . $TEST_BASE_DIR/test-functions
 
+test_run() {
+    ret=1
+    systemctl daemon-reload
+    systemctl start testsuite.service || return 1
+    systemctl status --full testsuite.service
+
+    if run_nspawn; then
+        check_result_nspawn || return 1
+    else
+        dwarn "can't run systemd-nspawn, skipping"
+    fi
+    test -s /failed && ret=$(($ret+1))
+    [[ -e /testok ]] && ret=0
+    return $ret
+}
+
 test_setup() {
-    create_empty_image_rootdir
+    mkdir -p $TESTDIR/root
+    initdir=$TESTDIR/root
+    STRIP_BINARIES=no
 
     # Create what will eventually be our root filesystem onto an overlay
     (
         LOG_LEVEL=5
-        eval $(udevadm info --export --query=env --name=${LOOPDEV}p2)
 
         setup_basic_environment
 
@@ -22,13 +40,25 @@ Description=Testsuite service
 After=multi-user.target
 
 [Service]
-ExecStart=/bin/sh -e -x -c 'systemctl --state=failed --no-legend --no-pager > /failed ; systemctl daemon-reload ; echo OK > /testok'
+ExecStart=/bin/sh -e -x -c 'systemctl --state=failed --no-pager > /failed ; systemctl daemon-reload ; echo SUSEtest OK > /testok'
 Type=oneshot
 EOF
 
         setup_testsuite
+        cp $initdir/etc/systemd/system/testsuite.service /etc/systemd/system/testsuite.service
     )
+
     setup_nspawn_root
+    rm -r $TESTDIR/root
+}
+
+test_cleanup() {
+    for service in testsuite.service; do
+        rm /etc/systemd/system/$service
+    done
+    [[ -e /testok ]] && rm /testok
+    [[ -e /failed ]] && rm /failed
+    return 0
 }
 
 do_test "$@"
