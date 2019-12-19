8ae8dcb28bccb2e207a76f0c4ff82bab69d8097a
Thomas Blume <Thomas.Blume@suse.com>
2019-12-19 09:00:32 +0100

    TEST-17-UDEV-WANTS: adapt for SUSE openQA

--- test/TEST-17-UDEV-WANTS/test.sh
+++ test/TEST-17-UDEV-WANTS/test.sh
@@ -3,18 +3,35 @@ set -e
 TEST_DESCRIPTION="UDEV SYSTEMD_WANTS property"
 TEST_NO_NSPAWN=1
 
+export TEST_BASE_DIR=/var/opt/systemd-tests/test
 . $TEST_BASE_DIR/test-functions
-QEMU_TIMEOUT=300
+
+test_run() {
+    ret=1
+    systemctl daemon-reload
+    systemctl start testsuite.service || return 1
+    systemctl status --full testsuite.service
+    if [ -z "$TEST_NO_NSPAWN" ]; then
+        if run_nspawn; then
+            check_result_nspawn || return 1
+        else
+            dwarn "can't run systemd-nspawn, skipping"
+        fi
+    fi
+    test -s /failed && ret=$(($ret+1))
+    [[ -e /testok ]] && ret=0
+    return $ret
+}
 
 test_setup() {
-    create_empty_image_rootdir
+    mkdir -p $TESTDIR/root
+    initdir=$TESTDIR/root
+    STRIP_BINARIES=no
 
     (
         LOG_LEVEL=5
-        eval $(udevadm info --export --query=env --name=${LOOPDEV}p2)
 
         setup_basic_environment
-        mask_supporting_services
 
         # setup the testsuite service
         cat >$initdir/etc/systemd/system/testsuite.service <<EOF
@@ -23,12 +40,32 @@ Description=Testsuite service
 
 [Service]
 ExecStart=/bin/bash -x /testsuite.sh
+ExecStartPost=/bin/sh -x -c 'systemctl --state=failed --no-pager > /failed'
 Type=oneshot
+StandardOutput=kmsg
+StandardError=kmsg
 EOF
-        cp testsuite.sh $initdir/
+        cp testsuite.sh /
+
+        for service in testsuite.service; do
+            cp $initdir/etc/systemd/system/$service /etc/systemd/system/
+        done
 
         setup_testsuite
     )
+
+    mask_supporting_services
+
+}
+
+test_cleanup() {
+    for service in testsuite.service; do
+         rm /etc/systemd/system/$service
+    done
+    [[ -e /testok ]] && rm /testok
+    [[ -e /failed ]] && rm /failed
+    rm /testsuite.sh
+    return 0
 }
 
 do_test "$@"
