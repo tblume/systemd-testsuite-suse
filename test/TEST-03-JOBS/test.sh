4a7054b1d556968782a9e8cceba4cf9b4f0c690b
Thomas Blume <Thomas.Blume@suse.com>
2019-12-19 08:41:58 +0100

    TEST-03-JOBS: adapt for SUSE openQA

--- test/TEST-03-JOBS/test.sh
+++ test/TEST-03-JOBS/test.sh
@@ -3,6 +3,7 @@ set -e
 TEST_DESCRIPTION="Job-related tests"
 TEST_NO_QEMU=1
 
+export TEST_BASE_DIR=/var/opt/systemd-tests/test
 . $TEST_BASE_DIR/test-functions
 
 test_setup() {
@@ -14,9 +15,10 @@ test_setup() {
         eval $(udevadm info --export --query=env --name=${LOOPDEV}p2)
 
         setup_basic_environment
-        mask_supporting_services
+        mask_supporting_services_nspawn
 
         # setup the testsuite service
+        echo "testservice=$initdir/etc/systemd/system/testsuite.service"
         cat >$initdir/etc/systemd/system/testsuite.service <<EOF
 [Unit]
 Description=Testsuite service
