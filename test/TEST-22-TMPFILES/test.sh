d22fde3aea2e534c52ac8ce92f6ffc7dfc910a55
Thomas Blume <Thomas.Blume@suse.com>
2019-12-19 09:05:57 +0100

    TEST-22-TMPFILES:  adapt for SUSE openQA

--- test/TEST-22-TMPFILES/test.sh
+++ test/TEST-22-TMPFILES/test.sh
@@ -3,12 +3,28 @@ set -e
 TEST_DESCRIPTION="Tmpfiles related tests"
 TEST_NO_QEMU=1
 
+export TEST_BASE_DIR=/var/opt/systemd-tests/test
 . $TEST_BASE_DIR/test-functions
 
+test_run() {
+    ret=1
+    systemctl daemon-reload
+    systemctl start testsuite.service || return 1
+    systemctl status --full testsuite.service
+    test -s /failed && ret=$(($ret+1))
+    [[ -e /testok ]] && ret=0
+    return $ret
+}
+
 test_setup() {
+    mkdir -p $TESTDIR/root
+    initdir=$TESTDIR/root
+    STRIP_BINARIES=no
+
+    LOG_LEVEL=5
+
     # create the basic filesystem layout
     setup_basic_environment
-    mask_supporting_services
     inst_binary mv
     inst_binary stat
     inst_binary seq
@@ -17,15 +33,27 @@ test_setup() {
     inst_binary readlink
 
     # setup the testsuite service
-    cp testsuite.service $initdir/etc/systemd/system/
+    cp testsuite.service /etc/systemd/system/
     setup_testsuite
 
-    mkdir -p $initdir/testsuite
-    cp run-tmpfiles-tests.sh $initdir/testsuite/
-    cp test-*.sh $initdir/testsuite/
+    mkdir -p /testsuite
+    cp run-tmpfiles-tests.sh /testsuite/
+    cp test-*.sh /testsuite/
 
     # create dedicated rootfs for nspawn (located in $TESTDIR/nspawn-root)
     setup_nspawn_root
+
+    mask_supporting_services_nspawn
 }
+
+test_cleanup() {
+    for service in testsuite.service; do
+         rm /etc/systemd/system/$service
+    done
+    rm -r /testsuite
+    [[ -e /testok ]] && rm /testok
+    [[ -e /failed ]] && rm /failed
+    return 0
+ }
 
 do_test "$@"
